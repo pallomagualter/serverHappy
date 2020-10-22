db8794408f3c69a7fec790394657ea8e84a335a8
Palloma Gualter <pallomagualter@gmail.com>
2020-10-22 11:58:51 -0300

    orm

---
+++ src/database/migrations/1603378652609-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1603378652609 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express, { response } from 'express';
+import express from 'express';
+
+import './database/connection';
 
 const app = express();
 
 app.get('/users', (request, response) => {
-    return response.json({ message: 'I am fine!'})
+    return response.json({ message: 'Good Day!'})
 });
 
 app.listen(3333);
