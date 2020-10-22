0bcf2dc2b7d4ee27f5cc891f2f89c5dcf10cbe94
Palloma Gualter <pallomagualter@gmail.com>
2020-10-22 16:57:46 -0300

    server

--- src/database/migrations/1603378652609-create_orphanages.ts
+++ src/database/migrations/1603378652609-create_orphanages.ts
@@ -1,59 +1,55 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class createOrphanages1603378652609 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-       await queryRunner.createTable(new Table({
-           name: 'orphanages',
-           columns: [
-               {
-                    name: 'id',
-                    type: 'integer',
-                    unsigned: true,
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: 'increment',
-                },
-                {
-                    name: 'name',
-                    type: 'varchar'
-                },
-                {
-                    name: 'latitude',
-                    type: 'decimal',
-                    scale: 10, 
-                    precision: 2,
-                },
-                {
-                    name: 'longitude',
-                    type: 'decimal',
-                    scale: 10, 
-                    precision: 2,
-                },
-                {
-                    name: 'about',
-                    type: 'text',
-                },
-                {
-                    name: 'instructions',
-                    type: 'text',
-                },
-                {
-                    name: 'open_on_weekends',
-                    type: 'varchar',
-                },
-                {
-                    name: 'opening_hours',
-                    type: 'varchar',
-                },
-
-           ],
-       }))
+        await queryRunner.createTable(new Table({
+            name: 'orphanages',
+            columns: [
+            {
+                name: 'id',
+                type: 'integer',
+                unsigned: true,
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment',
+            },
+            {
+                name: 'name',
+                type: 'varchar',
+            },
+            {
+                name: 'latitude',
+                type: 'decimal',
+                scale: 10,
+                precision: 2,
+            },
+            {
+                name: 'longitude',
+                type: 'decimal',
+                scale: 10,
+                precision: 2,
+            },
+            {
+                name: 'about',
+                type: 'text',
+            },
+            {
+                name: 'instructions',
+                type: 'text',
+            },
+            {
+                name: 'open_on_weekends',
+                type: 'boolean',
+                default: false,
+            },
+            ]
+        }))
+    }
         // REALIZAR ALTERAÇÕES
         // CRIAR TABELA, CRIAR UM NOVO CAMPO, DELETAR ALGUM CAMPO
-    }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        // DESFAZER O QUE FOI FEITO NO UP
+        // DESFAZER O QUE FOI FEITO UP
         await queryRunner.dropTable('orphanages');
     }
 
