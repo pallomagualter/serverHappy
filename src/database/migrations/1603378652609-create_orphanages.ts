70ff5a5d997a2e004a872503ac6ddd9c4404982f
Palloma Gualter <pallomagualter@gmail.com>
2020-10-22 13:50:00 -0300

    migration orphanages

--- src/database/migrations/1603378652609-create_orphanages.ts
+++ src/database/migrations/1603378652609-create_orphanages.ts
@@ -1,11 +1,56 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { text } from "express";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createOrphanages1603378652609 implements MigrationInterface {
-
     public async up(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.createTable(new Table({
+           name: 'orphaneges',
+           columns: [
+               {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                },
+                {
+                    name: 'latitude',
+                    type: 'decimal',
+                    scale: 10, 
+                    precision: 2,
+                },
+                {
+                    name: 'longitude',
+                    type: 'decimal',
+                    scale: 10, 
+                    precision: 2,
+                },
+                {
+                    name: 'about',
+                    type: 'text',
+                },
+                {
+                    name: 'instructions',
+                    type: 'text',
+                },
+                {
+                    name: 'open_on_weekends',
+                    type: 'text',
+                },
+           ],
+       }))
+        // REALIZAR ALTERAÇÕES
+        // CRIAR TABELA, CRIAR UM NOVO CAMPO, DELETAR ALGUM CAMPO
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        // DESFAZER O QUE FOI FEITO NO UP
+        await queryRunner.dropTable('orphanages');
     }
 
 }
