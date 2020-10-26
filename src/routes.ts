8d0acc92bc30a6c32c5ba24239e3d486bf04aacd
Palloma Gualter <pallomagualter@gmail.com>
2020-10-26 15:34:59 -0300

    multer array images

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 // INDEX, SHOW, CREATE, UPDATE, DELETE
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create); //Para multiplas imagens de uma vez
     
 export default routes;
