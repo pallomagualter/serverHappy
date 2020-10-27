fc2309d6a021094082b54ad1907a1a0c85afe3a3
Palloma Gualter <pallomagualter@gmail.com>
2020-10-27 10:54:58 -0300

    validation

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -58,6 +58,25 @@ export default {
             open_on_weekends,
             images
         };
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
+        });
+
+        await schema.validate(data, {
+            abortEarly: false, //para retornar todos os erros e não simplesmente parar ao encontrar o primeiro erro
+        });
     
         const orphanage = orphanagesRepository.create(data);
     
