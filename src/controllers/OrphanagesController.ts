f63ce3d72047bf85bf410c4740735a7205568299
Palloma Gualter <pallomagualter@gmail.com>
2020-10-27 09:56:58 -0300

    view images

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
+import orphanageView from '../views/orphanages_view';
 
 export default { 
     //ele retorna um objeto
@@ -11,7 +12,7 @@ export default {
             relations: ['images']
         });
 
-        return response.json(orphanages);
+        return response.json(orphanageView.renderMany(orphanages));
     },
 
     async show(request: Request, response: Response){
@@ -23,7 +24,7 @@ export default {
             relations: ['images']
         });
 
-        return response.json(orphanage);
+        return response.json(orphanageView.render(orphanage));
     },
 
     async create(request: Request, response: Response) {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import './database/connection';
+import path from 'path';
 
 import routes from './routes';
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(3333);
 
